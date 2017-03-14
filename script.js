0af5aff607aafc13dcae1c2eb0f5981339a1b41c
Liz Rodriguez <ecrodriguez@gmail.com>
2017-03-14 16:39:23 -0400

    add start btn

--- script.js
+++ script.js
@@ -2,19 +2,23 @@ jQuery(function() {
 
 //create board and show rules
 
-  function showRules(){
-
-  }
-
-
-
   function displayBoard(){
-
+    // let $startButton = $('button#start');
+    let $startButton = $('button#start');
+    //if start button is pressed, hide rules then display empty cafe
+    $('button#start').click(function() {
+      $('div#rules').hide();
+      console.log("button clicked");
+    });
   }
 
-  function createZombies(){
+  displayBoard();
 
-  }
+    //display background stuff
+
+  // function createZombies(){
+
+  // }
 
 
 });
