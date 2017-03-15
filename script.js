11ad1c67c830e135bf34ec9f9a9cbd48f74de12e
Liz Rodriguez <ecrodriguez@gmail.com>
2017-03-15 10:36:56 -0400

    add angry zombie

--- script.js
+++ script.js
@@ -1,34 +1,38 @@
 jQuery(function() {
   //create board and show rules
-  function displayBoard(){
-    $('div#board').hide();  //hide cafe board by default
-    //if start button is pressed
-    $('button#start').click(function() {
-      $('div#rules').hide(); //hide rules
-      $('div#board').show(); //then display empty cafe
-      console.log("start button clicked");
-    });
-  }
-  displayBoard();
+    function displayBoard(){
+      $('div#board').hide();  //hide cafe board by default
+      //if start button is pressed
+      $('button#start').click(function() {
+        $('div#rules').hide(); //hide rules
+        $('div#board').show(); //then display empty cafe
+        console.log("start button clicked");
+      });
+    }
+    displayBoard();
 
     $('button#ready').click(function() {
     console.log("ready button clicked");
     $('p#board').remove();
     $('button#ready').remove();
-    createZombie();
+    createZombie(); //create a look to get extra zombies?
     });
 
-  function createZombie(){
-    let $zombies = $('div').last().addClass('zombies');
-    $zombies.appendTo($('<div id="board>'));
+    function createZombie(){
+      let $zombies = $('div').last().addClass('zombiein');
+      $zombies.appendTo($('<div id="board>'));
+      $zombies.css('top', Math.random() * 400);
+      $zombies.css('left', Math.random() * 600);
+      setTimeout(function(){
+      angryZombie();
+      }, 3000);
+    }
 
-    // $zombies.first($('<div id="board" style="display: block;">'));
-    // setTimeout(function(){
-    // $('div#zombie').show();
-    // }, 2000); //hide the p text after 5 seconds
-  }
-  // createZombie();
+    function angryZombie(){
+      //after a few seconds, show zombieout
+      let $zombies = $('div').last().addClass('zombieout');
+      $zombies.appendTo($('<div id="board>'));
 
-
+    }
 
 });
