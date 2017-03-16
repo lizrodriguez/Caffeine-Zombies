70a0e4556c99b63df1dc48d97fc33b13d2e4d309
Liz Rodriguez <ecrodriguez@gmail.com>
2017-03-15 20:35:38 -0400

    add extra zombie state, timer

--- script.js
+++ script.js
@@ -1,45 +1,93 @@
 jQuery(function() {
   //create board and show rules
-    function displayBoard(){
-      $('div#board').hide();  //hide cafe board by default
-      //if start button is pressed
-      $('button#start').click(function() {
-        $('div#rules').hide(); //hide rules
-        $('div#board').show(); //then display empty cafe
-        console.log("start button clicked");
-      });
-    }
-    displayBoard();
+  let score = {barista: 0, zombie: 0}; //object to keep score
 
-    $('button#ready').click(function() {
-    console.log("ready button clicked");
-    $('p#board').remove();
-    $('button#ready').remove();
-    createZombie(); //create a look to get extra zombies?
+  let timer = 10;
+  let zombieSpawn;
+
+  function displayBoard(){
+    $('div#board').hide();  //hide cafe board by default
+    //if start button is pressed
+    $('button#start').click(function() {
+      $('div#rules').hide(); //hide rules
+      $('div#board').show(); //then display empty cafe
+      // console.log("start button clicked");
+      $('#gameover').hide();
     });
+  }
+  displayBoard();
 
-    function createZombie(){
-      let $zombies = $('div').last().addClass('zombiein');
-      $zombies.appendTo($('<div id="board>'));
-      $zombies.css('top', Math.random() * 400);
-      $zombies.css('left', Math.random() * 600);
-      setTimeout(function(){
-      angryZombie();
-      }, Math.random()*5000); //after a few seconds, show zombieout
+  $('button#ready').click(function() {
+    // console.log("ready button clicked");
+    $('#boardInstruction').remove();
+    $('button#ready').remove();
+
+    $('#timer').text('Timer: ' + timer);
+    setTimeout(countdownByOne, 1000);
+    // createZombie(); //create a loop to get extra zombies?
+
+    function countdownByOne(){
+      timer -= 1;
+      $('#timer').text('Timer: ' + timer);
+
+      if(timer > 0){
+        setTimeout(countdownByOne, 1000);
+      }
+      else {
+        $('#gameover').show();
+        clearInterval(zombieSpawn); //stops zombies from spawning
+      }
+
+
     }
 
-    function angryZombie(){
-      let $zombies = $('div').last().addClass('zombieout');
-      $zombies.appendTo($('<div id="board>'));
+    zombieSpawn = setInterval(function(){
+      createZombie();
+      }, 2500);
+  });
+
+  function createZombie(){
+  // let $zombies = $('div').last().addClass('zombiein');
+  let $zombies = $('<div class="zombiein"></div>');
+  $zombies.appendTo($('#board'));// $zombies.appendTo($('<div id="board>'));
+  let x = Math.random() * 600;
+  let y = Math.random() * 400;
+
+  $zombies.css('top', y);
+  $zombies.css('left', x);
+
+  console.log('x : ' + x + ' y : ' + y);
+
+    setTimeout(function(){
+    angryZombie($zombies);
+    }, Math.random()*5000); //after random amount of time, show zombieout/angry zombie
+  }
+
+  function angryZombie(zombies){
+    if (timer ===0){
+      return; //will exit if game is over (timer ===0)
     }
 
-    function shootZombie(){
-      let $zombies = $('div').last();
-      $zombies.appendTo($('<div id="board>'));
+    zombies.addClass('zombieout');
+  }
+
+  // //if zombie doesn't get hit after 3 seconds, then display zombieAngrier increase its score
+  // score.zombie+=10;
+  // console.log("zombie score: " + score.zombie);
+
+  function shootZombie(){
+    let $zombies = $('div').last();
+    $zombies.appendTo($('<div id="board>'));
       $zombies.on("click", function(){
-      $zombies.addClass('zombiehappy');
+        $zombies.addClass('zombiehappy');
+        setTimeout(function(){
+        $zombies.remove();
+        }, 1000);
+        score.barista+=10;
+        console.log("barista score: " + score.barista);
+       //count points towards you/barista
       });
-    }
-shootZombie();
+  }
+  //shootZombie();
 
 });
